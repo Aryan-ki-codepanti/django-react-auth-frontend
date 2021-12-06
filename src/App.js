81a43892097625237a4d3bc97e5cd3519e6f97ea
Aryan Sethi <aryan.rdps@gmail.com>
2021-12-06 22:56:55 +0530

    Added Header and  Private Route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
 import Home from "./pages/Home";
-import { BrowserRouter as Router ,Route , Routes } from "react-router-dom"; 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Header from "./components/Header";
+import PrivateRoute from "./utils/PrivateRoute";
+
 function App() {
     return (
         <div className="App">
-			<Router>
-				<Routes>
-					<Route path="/" element={ <Home /> } />
-					<Route path="/login" element={ <Login /> } />
-				</Routes>
-			</Router>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/login"
+                        element={
+                            <PrivateRoute>
+                                <Login />
+                            </PrivateRoute>
+                        }
+                    />
+                </Routes>
+            </Router>
         </div>
     );
 }
---
+++ src/utils/PrivateRoute.jsx
@@ -0,0 +1,9 @@
+
+const PrivateRoute = ({children}) => {
+    console.log("PrivateRoute");
+    return (
+        {...children}
+    )
+}
+
+export default PrivateRoute
