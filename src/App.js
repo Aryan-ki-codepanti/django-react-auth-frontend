32db8c4514ee10ca9f7aa07c4f4e15c10006ca63
Aryan Sethi <aryan.rdps@gmail.com>
2021-12-06 22:43:13 +0530

    Added Home and Login page with routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
-
+import Home from "./pages/Home";
+import { BrowserRouter as Router ,Route , Routes } from "react-router-dom"; 
+import Login from "./pages/Login";
 function App() {
     return (
         <div className="App">
-            <h1>Home</h1>
+			<Router>
+				<Routes>
+					<Route path="/" element={ <Home /> } />
+					<Route path="/login" element={ <Login /> } />
+				</Routes>
+			</Router>
         </div>
     );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div>
+            <form>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="Enter Username"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter Password"
+                />
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+    );
+};
+
+export default Login;
